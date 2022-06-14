89deef7f1980b7978955e3a75939c9ee2632936e
vamejiari <vameri98@gmail.com>
2022-06-14 16:14:33 -0500

    .

--- back/app.js
+++ back/app.js
@@ -12,7 +12,12 @@ const app=express()
 //CONFIGURACION CORS
 app.use(cors())
 app.use(express.json())
+
+//RUTA DEPARTAMENTO
 app.use('/departamentos', RoutesDepartamento)
+
+//RUTA MUNICIPIO
+
 
 //DEFINIR CONEXION A LA BASE DE DATOS
 try {
@@ -22,10 +27,7 @@ try {
     console.log(`El error de conexion es: ${error}`)
 }
 
-/*app.get('/',(req, res)=>{
-    res.send('Hola mundo')
-})*/
-
+//SERVIDOR
 app.listen(5500,()=>{
     console.log('Servidor levantado y funcionando en el puerto http://localhost:5500/')
 })
